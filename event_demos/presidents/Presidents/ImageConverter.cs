c1446647adeee7bef7f0f293b3b2a98e814805fe
Nikola Metulev <nikolame@microsoft.com>
2016-08-29 20:13:08 -0700

    added Presidents demo

---
+++ event_demos/presidents/Presidents/ImageConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media.Imaging;
+
+namespace Presidents
+{
+    public sealed class ImageConverter : IValueConverter
+    {
+        public ImageConverter() { }
+
+        public object Convert(object value, Type targetType,
+                              object parameter, string culture)
+        {
+            if (value is string)
+                return new BitmapImage(new Uri((string)value));
+            else if (value is Uri)
+            {
+                return new BitmapImage((Uri)value);
+            }
+            else
+                return null;
+        }
+
+        public object ConvertBack(object value, Type targetType,
+                                  object parameter, string culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ event_demos/presidents/Presidents/IsTenFootTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.System.Profile;
+using Windows.UI.Xaml;
+
+namespace Presidents
+{
+    // TODO: 1.3 - See MainPage.xaml for Ten Foot View Changes 
+    // TODO: 1.4 - See the XAML for adding a template for when IsTenFootTrigger fires
+    public class IsTenFootTrigger : StateTriggerBase
+    {
+        public IsTenFootTrigger()
+        {
+            SetActive(App.IsTenFoot);
+
+            App.TenFootModeChanged += App_TenFootModeChanged;
+        }
+
+        private void App_TenFootModeChanged(object sender, EventArgs e)
+        {
+            SetActive(App.IsTenFoot);
+        }
+    }
+}

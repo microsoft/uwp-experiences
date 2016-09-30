d7bcfce9adec0249b88ab1435345fdaa5deeef34
Nikola Metulev <Nikola Metulev>
2016-09-29 22:48:27 -0700

    added South Ridge app

---
+++ README.md
@@ -0,0 +1,9 @@
+# South Ridge Video (video app)
+
+[![SouthRidge](http://imgur.com/3lSmSJd.png)](https://www.youtube.com/watch?v=Gj8IWdeYunQ)
+
+South Ridge Video, an open source video app developed as a hosted web application built with React.js, and hosted on a web server. South Ridge can easily be converted to a UWP application that takes advantage of native platform capabilities and can be distributed through the Windows Store as any other UWP app. 
+
+Read the [full blog post](https://blogs.windows.com) or visit the [App Dev on Xbox landing page](http://aka.ms/xboxappdev) for more videos and blogs.
+
+App is hosted at [http://southridge.azurewebsites.net](http://southridge.azurewebsites.net)
---
+++ SouthRidgeVideo/frontpage.js
@@ -0,0 +1,405 @@
+ 
+ 
+ 
+ var data = [
+   {section:"recently released", id:"A", movies:[
+   {title:"Movie 1", id:1, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image09.jpg"},
+   {title:"Movie 1", id:2, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image14.jpg"},
+   {title:"Movie 1", id:3, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image19.jpg"},
+   {title:"Movie 1", id:4, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image26.jpg"},
+   {title:"Movie 1", id:5, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image21.jpg"},
+   {title:"Movie 1", id:6, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image29.jpg"},
+   {title:"Movie 1", id:7, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image39.jpg"},
+   {title:"Movie 1", id:8, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image27.jpg"},
+   {title:"Movie 1", id:9, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image15.jpg"},
+   {title:"Movie 1", id:10, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image11.jpg"},
+ ]},
+ {section:"Adventures", id:"B",movies:[
+   {title:"Movie 1", id:11, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image06.jpg"},
+   {title:"Movie 1", id:12, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image40.jpg"},
+   {title:"Movie 1", id:13, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image32.jpg"},
+   {title:"Movie 1", id:14, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image28.jpg"},
+   {title:"Movie 1", id:15, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image49.jpg"},
+   {title:"Movie 1", id:16, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeaturedImage_2x1_Image03.jpg"},
+   {title:"Movie 1", id:17, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image10.jpg"},
+   {title:"Movie 1", id:18, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image19.jpg"},
+   {title:"Movie 1", id:19, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image22.jpg"},
+   {title:"Movie 1", id:20, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image09.jpg"}
+   
+ ]},
+ {section:"People",id:"C", movies:[
+   {title:"Movie 1", id:21, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image13.jpg"},
+   {title:"Movie l", id:22, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image16.jpg"},
+   {title:"Movie k", id:23, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image17.jpg"},
+   {title:"Movie l", id:24, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image22.jpg"},
+   {title:"Movie k", id:25, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image07.jpg"},
+   {title:"Movie a", id:26, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image19.jpg"},
+   {title:"Movie b", id:27, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image14.jpg"},
+   {title:"Movie c", id:28, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image08.jpg"},
+   {title:"Movie d", id:29, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image28.jpg"},
+   {title:"Movie e", id:30, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image27.jpg"}
+ ]},
+ {section:"The Great Outdoors",id:"D", movies:[
+   {title:"Movie k", id:31, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image48.jpg"},
+   {title:"Movie l", id:32, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image34.jpg"},
+   {title:"Movie k", id:33, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image22.jpg"},
+   {title:"Movie l", id:34, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image08.jpg"},
+   {title:"Movie k", id:35, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image18.jpg"},
+   {title:"Movie a", id:36, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image25.jpg"},
+   {title:"Movie b", id:37, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image26.jpg"},
+   {title:"Movie c", id:38, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image09.jpg"},
+   {title:"Movie d", id:39, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image27.jpg"},
+   {title:"Movie e", id:40, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image28.jpg"}
+ ]},
+ {section:"Kids",id:"E", movies:[
+   {title:"Movie k", id:41, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image37.jpg"},
+   {title:"Movie l", id:42, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image45.jpg"},
+   {title:"Movie k", id:43, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image12.jpg"},
+   {title:"Movie l", id:44, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image59.jpg"},
+   {title:"Movie k", id:45, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image24.jpg"},
+   {title:"Movie a", id:46, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image13.jpg"},
+   {title:"Movie b", id:47, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image21.jpg"},
+   {title:"Movie c", id:48, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image22.jpg"},
+   {title:"Movie d", id:49, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image23.jpg"},
+   {title:"Movie e", id:50, imgUrl:"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image24.jpg"}
+ ]}
+   ]
+   
+ //custom events
+ var loadMoviePage = new CustomEvent('loadMoviePage');
+ var playMovieFullScreen = new CustomEvent('playMovieFullScreen');
+ var currentPlayObj = {title:'Galaxy Explorer', imgUrl:'http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image04.jpg'};
+ 
+  var initStateObj = { state: "frontPage" };
+history.pushState(initStateObj, "page 2", "#frontPage");
+
+ 
+var loadMovieState = function(a){
+
+      var stateObj = { state: "playMovie" };
+history.pushState(stateObj, "page 2", "#playMovie");
+document.dispatchEvent(loadMoviePage)
+};
+
+
+ 
+ 
+    
+var HeroBox = React.createClass({
+  handleClick() {
+     currentPlayObj = this.props;
+    loadMovieState()
+  },
+    handleFocus(){
+    //  window.scrollTo(0,0)
+    //console.log('should be focusing top')
+     $("html, body").animate({ scrollTop: 0 }, 300);
+  },
+  render: function() {
+    return (
+      <div className="heroBox">
+      <div className="controls">
+            <h1>Galaxy Adorer</h1>
+                <button onFocus={this.handleFocus} onClick={this.handleClick} ><span className="arrow-right">▷</span></button>   
+        </div>
+      </div>
+    );
+  }
+});
+
+var MovieBox = React.createClass({
+    handleClick: function() {
+          currentPlayObj = this.props.data;
+    loadMovieState()
+  },
+  render: function(){
+    var makeId = "m"+this.props.id
+    return(
+      <div className="movieBoxContainer">
+        <button className="movieBox" id={makeId} data-tv-focus-right={this.props.focusData} title={this.props.title} onClick={this.handleClick} style={{backgroundImage: 'url('+this.props.imgUrl+')'}}><span></span></button>
+        {this.props.title}
+      </div>
+      
+    )
+  }
+});
+
+
+var MovieSection = React.createClass({
+  handleClick(e){
+   var maxPos = $(e.currentTarget).siblings()[1].scrollWidth-$( e.currentTarget).siblings()[1].clientWidth;
+   var curPos = $(e.currentTarget).siblings()[1].scrollLeft;
+   var distance = $( e.currentTarget).siblings()[1].clientWidth-200;
+    if(curPos >= maxPos) distance = (curPos+distance)*-1;
+    $(e.currentTarget).siblings().animate({ scrollLeft: curPos+distance }, 600);
+
+  },
+  render: function(){
+    var i =0;
+    var leng = this.props.data.length;
+   var movieSectionNodes = this.props.data.map(function(data) {
+     i++;
+     if(leng == i){
+       leng = leng-1;
+      var pin = "#m"+(data.id-leng);
+      console.log("leng", leng);
+      console.log('data.id', data.id)
+     return (
+        
+          <MovieBox id={data.id} key={data.id} title={data.title} data={data} imgUrl={data.imgUrl} focusData={pin}/>
+        
+      );
+     }else{
+
+             return (
+        
+          <MovieBox id={data.id} key={data.id} title={data.title} data={data} imgUrl={data.imgUrl} />
+        
+      );
+     }
+
+    });
+    return(
+      <div className="movieSection" id={this.props.id}>
+       <h4>{this.props.section}</h4>
+       <div className="list">
+       {movieSectionNodes}
+       </div>
+       <button onClick={this.handleClick} className="overArrow">❯</button>
+      </div>
+    )
+  }
+});
+
+
+var MovieListing = React.createClass({
+    render: function() {
+      var num = 0;
+      var movieSectionNodes = this.props.data.map(function(data) {
+      return (
+        
+          <MovieSection title={data.title} key={data.id} section={data.section} data={data.movies} />
+        
+      );
+    });
+
+    return (
+      <div className="movieListing">
+       {movieSectionNodes}
+       
+      </div>
+    );
+  }
+});
+
+class MoviePlayer extends React.Component {
+  constructor() {
+    super();
+   };
+  componentDidMount(){
+
+    initializePlayer()
+  
+  }
+  render(){
+    return(
+      <div id="videoContainer" className="moviePlayer">
+ 	      <video id="myVideo" controls="true" width="100%" height="100%" autoPlay>
+ 		      <source src="http://video.ch9.ms/ch9/2f52/44b36f3c-0822-40b1-9926-6771225a2f52/mjsNapol01_high.mp4" type="video/mp4"></source>
+ 	      </video>
+	<div id="videoControls" className="controls">
+		
+		<div className="col span-12 center-content">
+			<input id="seekSlider" type="range" min="0" max="100" defaultValue="0" step="1" />
+		</div>
+		
+		<div className="col span-12 center-content">
+			<div className="col span-6 left-controls">
+				<span id="curTimeText">0:00:00</span>
+			</div>
+			<div className="col span-6 right-controls">
+				<span id="remTimeText">-0:00:00</span>
+			</div>
+		</div> 
+
+
+		<div className="col span-12 center-controls">
+			<button id="closeCaptionBtn" className="iconfont">&#xEC15;</button>
+			<button id="aspRatioBtn" className="iconfont">&#xE799;</button>
+			<button id="playPauseBtn" className="iconfont">&#xE769;</button>
+			<button id="volumeBtn" className="iconfont">&#xE15D;</button>
+			<button id="fullscreenBtn" className="iconfont">&#xE1D9;</button>
+		</div>
+	</div>
+	<div id="volumeModal">
+		<button id="muteBtn" className="iconfont">&#xE15D;</button>
+		<input id="volumeSlider" className="iconfont" type="range" min="0" max="100" defaultValue="100" step="1" />
+	</div>
+</div>
+      
+    )}
+};
+
+var PlayMovie = React.createClass({
+  render: function(){
+    //<div className="moviePlayer" style={{backgroundImage: 'url('+this.props.data.imgUrl+')'}}></div>
+    
+    return(
+      <div className="playMovie">
+        <MoviePlayer />
+        <div className="movieText">
+            <h2>{this.props.data.title}</h2>
+            <div className="movieTextWrapper">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fermentum ipsum sit amet porta faucibus. Cras ullamcorper a odio non rutrum.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fermentum ipsum sit amet porta faucibus. Quisque a diam id tellus placerat euismod.</p>
+
+              <p className="playInfo">2008  |  1 hr 51 min</p>
+           </div>
+        </div>
+      </div>
+    )
+  }
+});
+
+var PlayMovieDetails = React.createClass({
+  render:function(){
+   // console.log('movie obj', this.props)
+    return(
+      <div className="movieText">
+            <h2>{this.props.data.title}</h2>
+           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fermentum ipsum sit amet porta faucibus. Quisque a diam id tellus placerat euismod. Cras ullamcorper a odio non rutrum.</p>
+           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fermentum ipsum sit amet porta faucibus. Quisque a diam id tellus placerat euismod.</p>
+      </div>
+    )
+  }
+});
+
+var PlayMoveAdditional = React.createClass({
+  render: function(){
+    return(
+      <div className="additionalDetailWrapper">
+        <h4>Additional Details</h4>
+        <div className="lowerDetailsWrapper">
+          <div className="detailBlock">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fermentum ipsum sit amet porta faucibus</div>
+          <div className="detailBlock">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fermentum ipsum sit amet porta faucibus</div>
+        </div>
+      </div>
+      
+    )
+    
+  }
+  
+  
+})
+    
+class TopSection extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      viewer: 'frontPage'
+    };
+   };
+  componentDidMount(){
+    var that = this;
+    window.addEventListener('popstate', function(e){
+      var stateObj = e.state;
+      window.scrollTo(0, 0);
+      that.setState({viewer: stateObj.state});
+      
+    });
+    document.addEventListener('loadMoviePage', function(){
+      that.setState({viewer: 'playMovie'});
+     // console.log(currentPlayObj)
+    })
+  }
+  render() {
+   if (this.state.viewer === 'frontPage'){
+          return(
+      <div className="topSection" >
+      <HeroBox title={"Galaxy Explorer"} id={0} imgUrl={"http://adx.azureedge.net/Images/Clean/Small/FeatureImage_2x1_Image04.jpg"} />
+      </div>
+    )
+    }else{
+      return(
+      <div className="topSection" >
+      <PlayMovie data={currentPlayObj} />
+      </div>
+    )}
+  }};
+
+class BottomSection extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      viewer: 'frontPage'
+    };
+   };
+
+  componentDidMount(){
+    var that = this;
+    window.addEventListener('popstate', function(e){
+      var stateObj = e.state;
+      window.scrollTo(0, 0);
+      that.setState({viewer: stateObj.state});
+      
+    });
+    document.addEventListener('loadMoviePage', function(){
+      window.scrollTo(0, 0);
+      that.setState({viewer: 'playMovie'});
+    })
+  }
+  render() {
+ 
+ 
+   if (this.state.viewer === 'frontPage'){
+          return(
+      <div className="bottomSection" >
+      <MovieListing data={this.props.data} />
+      </div>
+    )
+    }else{
+      return(
+      <div className="bottomSection">
+      <PlayMoveAdditional data={currentPlayObj} />
+      </div>
+    )}
+  }};
+
+
+
+var LogoBox = React.createClass({
+  render: function() {
+    return (
+      <div className="logoBox">
+      <img src="http://adx.metulev.com/video/Logo/Logo_NoText.png" className="logo" />
+      <div>Film</div>
+      <div>Broadcast</div>
+      <div>Commercials</div>
+      <img src="" className="search" />
+      </div>
+    )
+  }
+});
+
+    
+var PageWrapper = React.createClass({
+  render: function() {
+    return (
+      <div className="pageWrapper">
+        <LogoBox />
+        <div className="topSection">
+        <TopSection />
+        </div>
+        <BottomSection data={this.props.data} />
+      </div>
+    );
+  }
+});
+
+
+
+
+ReactDOM.render(
+  <PageWrapper data={data}  />,
+  document.getElementById('page')
+);
